397ee3d8eb32bbedef594a7440cbd3b233532491
ykm11 <ushiromiya@gmail.com>
2024-06-10 23:29:55 +0900

    replaced reverse+push_back with resize+de-reference

--- wasserstein.h
+++ wasserstein.h
@@ -11,9 +11,16 @@ template <typename T> void argsort(const vector<T> &v, vector<size_t> &idx) {
 }
 
 template <typename T> void diff(const vector<T> &v, vector<T> &delta) {
+#if 0
   for (size_t i = 0; i < v.size() - 1; ++i) {
     delta.push_back(v[i + 1] - v[i]);
   }
+#else
+  delta.resize(v.size()-1);
+  for (size_t i = 0; i < v.size() - 1; ++i) {
+    delta[i] = v[i + 1] - v[i];
+  }
+#endif
 }
 
 template <typename T>
@@ -28,6 +35,7 @@ template <typename T>
 void searchSorted(const vector<T> &v, const vector<size_t> &idx,
                   const vector<T> &allValues, vector<size_t> &cdfIdx) {
   // for every element in all indices, find id in A
+#if 0
   cdfIdx.reserve(allValues.size());
   for (auto elem = allValues.begin(); elem != allValues.end() - 1; ++elem) {
     auto up = upper_bound(idx.begin(), idx.end(), *elem,
@@ -38,12 +46,27 @@ void searchSorted(const vector<T> &v, const vector<size_t> &idx,
       cdfIdx.push_back(up - idx.begin());
     }
   }
+#else
+  cdfIdx.resize(allValues.size());
+  size_t j = 0;
+  for (auto elem = allValues.begin(); elem != allValues.end() - 1; ++elem) {
+    auto up = upper_bound(idx.begin(), idx.end(), *elem,
+                          [&v](T elem, size_t i) { return elem < v[i]; });
+    if (up == idx.end()) {
+      cdfIdx[j] = idx.size();
+    } else {
+      cdfIdx[j] = up - idx.begin();
+    }
+    j++;
+  }
+#endif
 }
 
 template <typename T>
 void computeCDF(const vector<T> &weights, vector<size_t> &idx,
                 const vector<size_t> &cdfIdx, vector<T> &cdf) {
 
+#if 0
   vector<T> ordW;
   for (auto i : idx) {
     ordW.push_back(weights[i]);
@@ -57,6 +80,29 @@ void computeCDF(const vector<T> &weights, vector<size_t> &idx,
   for (auto cIdx : cdfIdx) {
     cdf.push_back(sortedAccW[cIdx] / accum);
   }
+#else
+  vector<T> ordW(idx.size());
+  size_t j = 0;
+  for (auto i : idx) {
+    ordW[j] = weights[i];
+    j++;
+  }
+
+  j = 0;
+  cdf.resize(cdfIdx.size());
+  vector<T> sortedAccW = {0};
+  partial_sum(ordW.begin(), ordW.end(), ordW.begin());
+  sortedAccW.insert(sortedAccW.end(), ordW.begin(), ordW.end());
+  T accum = sortedAccW.back();
+  for (auto cIdx : cdfIdx) {
+    cdf[j] = sortedAccW[cIdx] / accum;
+    j++;
+  }
+
+  for (size_t i = 0; i < cdf.size(); i++) {
+      printf("%f\n", cdf[i]);
+  }
+#endif
 }
 
 template <typename T>
